31cd2facaae2cea8afe2eff127088f01e9ff395f
Rahul Shukla <16shuklarahul@gmail.com>
2018-06-10 15:51:02 +0530

    mlab db url added

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/express-adminlte').then(
+// mongoose.connect('mongodb://localhost:27017/express-adminlte').then(
+//     ()=> {console.log("Database Connected..")},
+//     err => {console.log("err",err)}
+// )
+
+mongoose.connect('mongodb://admin:admin123@ds253960.mlab.com:53960/node-express-adminlte').then(
     ()=> {console.log("Database Connected..")},
     err => {console.log("err",err)}
 )
